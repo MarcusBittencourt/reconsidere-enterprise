99982cd54d0d168af75cad284b99fedaa0f1d7cc
marcusbittencourt <m4rcu5.3i773nc0ur7@gmail.com>
2018-02-10 17:07:46 -0200

    prototipação da funcionalidade de medição de resíduos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  glass = 0; 
+  plastic = 0;
+  metal = 0;  
+  organic = 0;
+  papper = 0;
+  enterprise = {name: "Reconsidere"};
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { MeasurementsComponent } from './measurements/measurements.component';
-
+import {MatSliderModule} from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,13 @@ import { MeasurementsComponent } from './measurements/measurements.component';
     MeasurementsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSliderModule,
+    FormsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
