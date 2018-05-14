6d6a9acf7428981ae1647a814cb15e536f344f35
RaulSimon <raulcapistrano55@gmail.com>
2018-05-14 20:39:08 -0300

    modificação sign-in

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
+  email: string;
+  password: string;
+  name: string;
+
   constructor() { }
 
   ngOnInit() {
   }
+   signup(){
 
+   }
 }
